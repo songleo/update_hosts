6829ac683500702491214e63442b17ddc88a7e2f
leo <lisong1205@126.com>
2016-01-23 01:23:10 +0800

    new file:   update_hosts 	new file:   update_hosts.go

---
+++ update_hosts.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	//"regexp/syntax"
+	"runtime"
+)
+
+var hostsFile string
+var Log = fmt.Println
+
+func init() {
+	if runtime.GOOS == "windows" {
+		hostsFile = "C:\\Windows\\System32\\drivers\\etc\\hosts"
+		fmt.Println("OS is Windows\nhosts:", hostsFile)
+	} else {
+		hostsFile = "/etc/hosts"
+		fmt.Println("OS is Linux!\nhosts:", hostsFile)
+	}
+
+}
+
+func main() {
+
+	var (
+		resp *http.Response
+		re   *regexp.Regexp
+		err  error
+	)
+
+	urlList := []string{
+		"http://googleips-google.stor.sinaapp.com/hosts",
+		"http://blog.my-eclipse.cn/hosts.txt",
+	}
+
+	for _, url := range urlList {
+
+		resp, err = http.Get(url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode == http.StatusOK {
+			fmt.Println("OK, update your hosts......")
+			break
+		} else {
+			continue
+		}
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Sorry :(\nUpdate your hosts fail, the program will exit.\n")
+		os.Exit(-1)
+	}
+	hosts, err := ioutil.ReadFile(hostsFile)
+	hostsContext := string(hosts)
+	var pat = "(?s)#old hosts start.*#old hosts end"
+
+	re, _ = regexp.Compile(pat)
+	oldStr := re.FindAllStringSubmatch(string(hostsContext), -1)
+	if len(oldStr) == 0 {
+		hostsContext = "\n#old hosts start\n" + hostsContext + "\n#old hosts end\n"
+	} else {
+		hostsContext = oldStr[0][0]
+	}
+
+	file, err := os.OpenFile(hostsFile, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	file.WriteString(hostsContext)
+	file.WriteString("\n \n")
+
+	buf := make([]byte, 10240)
+	for {
+		numBytes, _ := resp.Body.Read(buf)
+		if numBytes == 0 {
+			break
+		}
+		file.WriteString(string(buf[:numBytes]))
+	}
+}
